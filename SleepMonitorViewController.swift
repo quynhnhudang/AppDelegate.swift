90f683f974bde58e68a6c59632565734c8e1d805
Quynh Nhu Dang <87718231+quynhnhudang@users.noreply.github.com>
2023-11-26 17:08:37 -0500

    Create SleepMonitorViewController.swift

---
+++ SleepMonitorViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SleepMonitorViewController: UIViewController {
+    private let sleepMonitorService = SleepMonitorService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Implement UI components and layout
+        // Use sleepMonitorService to fetch and display data
+    }
+}
